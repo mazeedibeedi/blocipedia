a797a129db42f13ea44401bcdecf01fac4766403
mazeedibeedi <a.almazidi92@gmail.com>
2017-07-18 10:23:13 +0300

    User story 4 faker (#4)
    * Merge commit
    
    * Seeded the database with Faker constants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Creating Users
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+users = User.all
+
+# Creating Wikis
+
+50.times do
+  Wiki.create!(
+    title: Faker::Lorem.word,
+    body: Faker::Lorem.paragraph,
+    user: users.sample
+  )
+end
+
+puts "Seeds finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
